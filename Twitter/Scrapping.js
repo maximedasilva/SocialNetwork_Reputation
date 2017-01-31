45b467c7e8543cb8a4cb5df4029ab8594f2846cd
maximedasilva <maximedasilva40@gmail.com>
2017-01-31 16:26:40 +0100

    modification of scrapper add jquery and py file

--- Twitter/Scrapping.js
+++ Twitter/Scrapping.js
@@ -1,8 +1,14 @@
-var http= require('https');
+funciton ScraptheTweets()
+{
+  $.ajax({
+    url: 'https://api.twitter.com/1.1/search/tweets.json?q=%23macron',
+        dataType: 'jsonp',
+        success: function(data) {
+                              }
+        }
+    });
+}
 
+$(document).ready(function(){
 
-
-var oAuth= new oAuth
-(
-
-)
+})
