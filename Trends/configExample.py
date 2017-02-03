ce428218d6f228525398787a88ade5e9b0f41e2b
maximedasilva <maximedasilva40@gmail.com>
2017-02-03 22:26:58 +0100

    Add google trends folder

---
+++ Trends/configExample.py
@@ -0,0 +1,7 @@
+google_username="yourMail@gmail.com"
+google_password="xxx"
+#name your file config.py, with your py files with trend imported in the same folder, and do
+
+
+# config = {}
+# execfile("config.py", config)
--- Twitter/Configexample.py
+++ Twitter/Configexample.py
@@ -2,7 +2,7 @@ access_key = "XXXXXXXX"
 access_secret = "XXXXXXXX"
 consumer_key = "XXXXXXXX"
 consumer_secret = "XXXXXXXX"
-#name your file config.py, with your py files in the same folder, and do
+#name your file config.py, with your py files with twitter imported in the same folder, and do
 
 
 # config = {}
