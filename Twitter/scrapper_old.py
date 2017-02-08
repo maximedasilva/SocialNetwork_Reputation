8cb7325b941a25e00e1c23b465b473d095f47281
maximedasilva <maximedasilva40@gmail.com>
2017-02-08 13:47:23 +0100

    modify filter on scrapper

--- Twitter/scrapper_old.py
+++ Twitter/scrapper_old.py
@@ -1,20 +1,23 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-
 config = {}
 execfile("config.py", config)
 #print(api.me().name)
+
 class stdOutListener(StreamListener):
+
     def on_data(self, data):
-        print data
+        print _cpt
+        _cpt=_cpt+1
         return True
 
     def on_error(self,status):
         print status
 if __name__=='__main__':
+    _cpt=1
     mystream=stdOutListener()
     auth=OAuthHandler(config["consumer_key"],config["consumer_secret"])
     auth.set_access_token(config["access_key"], config["access_secret"])
     stream=Stream(auth,mystream)
-    stream.filter(track=['macron'])
+    stream.filter(track=['Fillon'])
