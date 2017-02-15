ab575d0bf068b17e43564ed4e29407629adee450
maximedasilva <maximedasilva40@gmail.com>
2017-02-15 14:38:39 +0100

    modifying scrapper solving positioning problem

--- Twitter/scrapper_old.py
+++ Twitter/scrapper_old.py
@@ -8,18 +8,17 @@ execfile("config.py", config)
 #print(api.me().name)
 _cpt=1
 myfile="file.json"
-file=open(myfile,"wb")
+file=open(myfile,"ab")
 
 class stdOutListener(StreamListener):
     print str(_cpt)
     _cpt=_cpt+1
     def on_status(self, status):
-        print(status.text)
-    def on_data(self, data):
-        json_data = json.loads(data)
-        file.write(str(json_data))
+        #print(status.text)
+        if(status.coordinates!="None"):
+            #print status.coordinates
+            print status.place
         return True
-
     def on_error(self,status):
         print status
 if __name__=='__main__':
@@ -28,4 +27,4 @@ if __name__=='__main__':
     auth=OAuthHandler(config["consumer_key"],config["consumer_secret"])
     auth.set_access_token(config["access_key"], config["access_secret"])
     stream=Stream(auth,mystream)
-    stream.filter(track=['#macron'])
+    stream.filter(locations=[-4.9,42.43,51.6,90])
