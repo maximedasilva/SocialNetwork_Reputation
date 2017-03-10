cf4981819c39d13a3702885ff5981a657036b6c2
maximedasilva <maximedasilva40@gmail.com>
2017-03-10 17:28:10 +0100

    delete some useless files

--- Trends/configExample.py
+++
@@ -1,7 +0,0 @@
-google_username="yourMail@gmail.com"
-google_password="xxx"
-#name your file config.py, with your py files with trend imported in the same folder, and do
-
-
-# config = {}
-# execfile("config.py", config)
--- Trends/scrapper.py
+++
@@ -1,8 +0,0 @@
-from pytrends.request import TrendReq
-
-config = {}
-execfile("config.py", config)
-pytrend = TrendReq(config["google_username"], config["google_password"], custom_useragent='My Pytrends Script')
-pytrend.build_payload(kw_list=['macron', 'melenchon'])
-interest_over_time_df = pytrend.interest_over_time()
- 
