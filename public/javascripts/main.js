7dcbff863aff4893a97377278d47c0da22957965
maximedasilva <maximedasilva40@gmail.com>
2017-03-31 16:07:17 +0200

    add python shell

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,6 +7,13 @@ var cities = extension.fromStream(cityStream, {
     headers: true,
     delimiter: ';'
 });
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell('./Twitter/launch.py');
+
+pyshell.on('message', function (message) {
+
+    console.log(message);
+});
 var i = 0;
 var cityTab = new Array();
 var inseeTab=new Array();
