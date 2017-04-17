671e0ef78f84e5296ce306970866c3b6a3e396ac
maximedasilva <maximedasilva40@gmail.com>
2017-04-17 14:29:30 +0200

    push to heorku

--- nodeJS_App/app.js
+++ nodeJS_App/app.js
@@ -6,12 +6,15 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var everybody=require('./routes/everybody');
 var javascript = require('./public/javascripts/main');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
+app.use('/everybody', everybody);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
