e241c067002fdd886ee95d3c97992aacd5730494
maximedasilva <maximedasilva40@gmail.com>
2017-04-24 22:14:56 +0200

    modify app

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -35,7 +35,7 @@ cities.on("end", function() {
       console.log(cpt++);
       var locationByCityName = new lbn(data.ville, cityTab,data.candidat,regionJSON);
       locationByCityName.affectArguments();
-      if(cpt>56000)
+      if(cpt>66000)
       {
       locationByCityName.writeData();
     }
